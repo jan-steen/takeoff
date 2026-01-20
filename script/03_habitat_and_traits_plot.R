3f668976b5dabc256f571644a95e9b9998914c52
Jan Steen <jan.steen@uni-muenster.de>
2026-01-20 11:14:31 +0100

    trying out new natura2000 plots

--- script/03_habitat_and_traits_plot.R
+++ script/03_habitat_and_traits_plot.R
@@ -161,7 +161,6 @@ tree <- data |>
 ## remove NAs
 #tree <- tree[c(1:24),]
 
-pal
 ### bring the factors in the right order for plotting the colours
 tree$Habitat_quality_grouped <- factor(tree$Habitat_quality_grouped, 
                                levels = c("Biomass", "Land Cover", "Species detection", "Biogeochemical","Other"))
@@ -198,6 +197,124 @@ treemap(tree,
 hab | eur
 
 #ggsave("figures/habitats.pdf", width = 18, height = 10, units = "cm")
+
+
+
+
+
+### new natura 2000 stuff
+
+euro <- data |>
+  select(DOI, Habitat_type, Country_study_area, NATURA_2000, Habitat_quality_grouped, Habitat_quality_lvl2) |>
+  filter(!(is.na(Habitat_type)  & is.na(Country_study_area) & is.na(NATURA_2000) & is.na(Habitat_quality_grouped) & is.na(Habitat_quality_lvl2))) |>
+  group_by(DOI) |> 
+  fill(Habitat_type,.direction = "down") |>
+  fill(NATURA_2000,.direction = "down") |>
+  fill(Country_study_area,.direction = "down") |>
+  fill(Habitat_quality_grouped,.direction = "down") |>
+  fill(Habitat_quality_lvl2,.direction = "down") |>
+  ungroup() |>
+  left_join(meta, by = "DOI") |>
+  filter(Country_study_area %in% c("Belgium","Bulgaria","Czechia","Denmark","Estonia",
+                                   "Finland","France", "Germany", "Hungary", "Ireland",
+                                   "Italy", "Netherlands", "Portugal", "Republic of Serbia",
+                                   "Romania", "Serbia", "Spain", "Sweden")
+         | Country_study_area %in% "United Kingdom" & Year < 2020 ) |>
+  filter(NATURA_2000 == "yes")
+
+### simplify and sort the habitats
+euro
+euro$Habitat_type <- case_when(
+  euro$Habitat_type %in% c("grassland", "desert") ~ "Grassland",
+  euro$Habitat_type == "heathland" ~ "Heathland",
+  euro$Habitat_type %in% c("peatland", "wetland") ~ "Wetland",
+  TRUE ~ "Other"
+)
+
+euro$Habitat_type <- factor(euro$Habitat_type, 
+                               levels = c("Grassland", "Wetland", "Heathland", "Other"))
+euro$Habitat_quality_grouped <- factor(euro$Habitat_quality_grouped,
+                                          levels = c("Biomass", "Land Cover", "Species detection","Biogeochemical", "Other","missing"))
+
+
+eurobar <- ggplot(euro, aes(x = Habitat_type, fill = Habitat_quality_grouped))+
+  geom_bar(position = position_dodge2(preserve = "single"))+
+  scale_y_continuous(expand = c(0,0))+
+  theme_classic() +
+  labs(fill = "analysed trait", x = "inside NATURA 2000 areas", y = "Number of observations")+
+  scale_fill_manual(values = pal, guide = guide_legend(ncol = 2))+
+  theme(legend.position = "bottom",
+        legend.key.spacing.y = unit(0, "pt"))
+eurobar
+
+
+################################################################################
+
+### treemap of only NATURA 2000 plots
+
+eurotree <- euro |>
+  count(Habitat_quality_grouped, Habitat_quality_lvl2) |>
+  filter(Habitat_quality_grouped %in% c("Biomass", "Land Cover", "Biogeochemical", "Species detection", "Other"))
+
+## remove NAs
+#tree <- tree[c(1:24),]
+
+### bring the factors in the right order for plotting the colours
+eurotree$Habitat_quality_grouped <- factor(eurotree$Habitat_quality_grouped, 
+                                       levels = c("Biomass", "Land Cover", "Species detection", "Biogeochemical","Other"))
+
+pal2 =c(
+  "Biomass" = "#fb8072",
+  "Land Cover" = "#80b1d3",
+  "Species detection" = "#8dd3a2",
+  "Biogeochemical" = "#ccbada",
+  "Other" = "#eaeaea"
+)
+
+
+
+treemap(eurotree,
+        index = c("Habitat_quality_grouped", "Habitat_quality_lvl2"),
+        vSize = "n",
+        type = "index",
+        #   title = "Habitat quality traits assessed with UASs",
+        title = "",
+        align.labels = list(c("left","top"),c("center","center")),
+        algorithm = "squarified",
+        fontcolor.labels = "black", 
+        palette = pal2
+        
+)
+
+################################################################################
+
+### heatmap of only NATURA 2000 studies
+library(viridis)
+
+test <- euro |>
+  select(Habitat_type, Habitat_quality_grouped) |>
+  count(Habitat_type, Habitat_quality_grouped) 
+
+
+ggplot(data = test, aes(x = Habitat_type, y = Habitat_quality_grouped, fill = n)) + 
+  geom_tile() +
+  # scale_fill_gradient(low = "#fef0d9", high = "#cb181d") + ### if gradient scale should be used
+  scale_fill_viridis(discrete = F, option = "G", direction = -1) +
+  labs(x = "", y = "", fill = "occurences")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        panel.grid.major = element_blank())
+
+
+
+
+
+
+
+
+
+
+
 
 
 
