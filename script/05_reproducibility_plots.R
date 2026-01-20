61eb2462ed822beb0aa39139c59926a8728d56d5
Jan Steen <jan.steen@uni-muenster.de>
2026-01-20 14:21:07 +0100

    model availability

--- script/05_reproducibility_plots.R
+++ script/05_reproducibility_plots.R
@@ -9,7 +9,7 @@ library(patchwork)
 
 ### directly read the excel file. no need for two files: 
 excel <- read_excel("data/data_extraction/data_extraction_final.xlsx", skip = 1, na = c("","NA"))
-meta <- excel[,c(2:10,18:20,39:41)]
+meta <- excel[,c(2:10,18:20,39:42)]
 data <- excel[,c(2,11:17,21:37)]
 
 
@@ -18,8 +18,8 @@ meta <- filter(meta, !(is.na(Title)))
 
 ### make variables into factors
 str(meta)
-meta[,c(2,6,8:14)] <- meta[,c(2,6,8:14)] |>
-  mutate_if(sapply(meta[,c(2,6,8:14)], is.character), as.factor)
+meta[,c(2,6,8:16)] <- meta[,c(2,6,8:16)] |>
+  mutate_if(sapply(meta[,c(2,6,8:16)], is.character), as.factor)
 
 
 str(data)
@@ -47,6 +47,9 @@ meta$Data_available <- factor(meta$Data_available,
 meta$Script_available <- factor(meta$Script_available,
                                 levels = c("yes", "partial", "on request","no"))
 
+meta$model_available<- factor(meta$model_available,
+                              levels = c("yes", "no", "not applicable"))
+
 ana <- ggplot(data[!(is.na(data$Analysis_open_source)),], aes(x= Analysis_open_source))+
   geom_bar(width = 0.6)+
   theme_classic() +
@@ -71,69 +74,33 @@ dat <- ggplot(meta[!(is.na(meta$Data_available)),], aes(x= Data_available))+
   labs(x = "Data available", y = "Number of observations")+
   scale_y_continuous(expand = c(0,0), limits = c(0,225))
 
+mod <- ggplot(meta[!(is.na(meta$model_available)),], aes(x= model_available))+
+  geom_bar(width = 0.75)+
+  theme_classic() +
+  labs(x = "Model available", y = "")+
+  scale_y_continuous(expand = c(0,0))
+
 ### try to make the width the same for all plots despite having 3 or 4 categories
 
-(ana | pho )/( dat | scr)
+(ana | pho | plot_spacer())/( dat | scr | mod)
 
-#ggsave("figures/reproducibility.pdf", width = 14, height = 12, units = "cm")
+#ggsave("figures/reproducibility.pdf", width = 18, height = 12, units = "cm")
 
 ################################################################################
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-repro <- excel |>
-  select(DOI, Data_available, Script_available) |>
-  filter(!(is.na(Script_available)))
-
-
-repro |> 
-  tidyr::pivot_longer(
-    cols = -DOI,
-    names_to = "data_or_script",
-    values_to = "availability"
-  ) |> 
-  ggplot(aes(x = data_or_script, fill = availability))+
-  geom_bar(position = "dodge")+
-  theme_minimal()+
-  labs(x = "", y = "count")
-
-
-
-open <- excel |>
-  select(DOI, Photogrammetry_open_source, Analysis_open_source) |>
-  filter(!(is.na(Photogrammetry_open_source) & is.na(Analysis_open_source))) |>
-  group_by(DOI) |>
-  fill(Photogrammetry_open_source,.direction = "down") |>
-  fill(Analysis_open_source,.direction = "down")
-
-
-
-open |> 
-  tidyr::pivot_longer(
-    cols = -DOI,
-    names_to = "test",
-    values_to = "Open_Source"
-  ) |> 
-  ggplot(aes(x = test, fill = Open_Source))+
-  geom_bar(position = "dodge")+
-  theme_minimal()+
-  labs(x = "", y = "count")
-
 table(data$Photogrammetry_open_source)
+
 table(data$Analysis_open_source)
+
 table(meta$Script_available)
+
 table(meta$Data_available)
+
+table(meta$model_available)
+
+
+
+
+
+
